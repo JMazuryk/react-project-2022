3669d0084e259e51373b3a6460f5647fd35ece62
JMazuryk <Julia.Mazuryk@gmail.com>
2022-07-23 11:58:34 +0300

    cw 5/2 componets

--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
 
 class User extends Component {
+
+    constructor(props) {
+        super(props);
+        this.item=this.props.item;
+    }
+
     render() {
         return (
             <div>
-                
+                {this.item.id} {this.item.body}
             </div>
         );
     }
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,21 +1,28 @@
 import React, {Component} from 'react';
+
 import {UserApiService} from "../services/user.api.service";
+import User from "./User";
 
 class Users extends Component {
-
+     //usestate -> state
     state = {users: []}
 
     constructor(props) {
+        console.log('constructor')
         super(props);
-        this.userApiServices = new UserApiService();
-        this.userApiServices.getUsers().then(value => this.setState({users: value}));
-    }
-
-
+     }
+//useEffect
+componentDidMount() {
+    console.log('componentDidMount')
+    this.userApiServices = new UserApiService();
+    this.userApiServices.getUsers().then(value => this.setState({users: value}));
+}
+//return -> render
     render() {
+        console.log('render')
         return (
             <div>
-                {this.state.users.map(value => <div>{value.title}</div>)}
+                {this.state.users.map(value => <User key={value.id} item={value}></User>)}
             </div>);
     }
 }
